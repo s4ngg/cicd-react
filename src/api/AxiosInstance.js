4f5b1adb47843ad66335200c656b823a6c6f41cc
s4ngg <s4ngg@naver.com>
2026-03-19 17:48:30 +0900

    te

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-
 const axiosInstance = axios.create({
     baseURL: 'https://d5ag0ambmwchd.cloudfront.net',
     timeout: 10000,
-    header: {
+    headers: {
         'Content-Type': 'application/json'
     }
 })
